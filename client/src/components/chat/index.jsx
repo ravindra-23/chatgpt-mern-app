cfdc4088f8209cfd1253e4d4aa3de49837590f0a
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-18 14:40:17 +0530

    added aicode condition

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
 import Header from '@/components/customHeader'
 import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm'
+import Ai from '@/components/customMessageForms/Ai'
+import AiCode from '@/components/customMessageForms/AiCode'
 
 const Chat = () => {
 
@@ -18,6 +20,13 @@ const Chat = () => {
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+
+          if(chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />
+          }
+          if(chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />
+          }
           return (
             <StandardMessageForm props={props} activeChat = {chatProps.chat} />
           )
