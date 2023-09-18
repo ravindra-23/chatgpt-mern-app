13773a2ea66a1be3d6cbb0de6987bd2637022595
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-18 14:58:34 +0530

    added home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Chat from "@/components/chat"
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
       <div className="app">
         <Router>
           <Routes>
-            <Route path="/chat" element={ <Chat /> } />
+            <Route path="/" element={ <Chat /> } />
           </Routes>
         </Router>
       </div>
