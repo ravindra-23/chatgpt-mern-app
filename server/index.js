560c605b369c40e36f60328927f354d6bfaafd19
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-19 13:04:34 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://chatgpt-mern-app-zjrl.vercel.app",
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 /* OPEN AI CONFIGURATION */
 
