aeb652133998b824d9b9ee3d15ec0c6f7a41c7cd
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-24 00:57:30 +0530

    added config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    chunkSizeWarningLimit: 1000, // Increase the warning limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': ['react', 'react-dom'], // Example of manual chunking
+        },
+      },
+    },
+  },
 })
